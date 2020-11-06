9fc334d153265e40c095019bb4d2f74605eabb08
KatarzynaGorczyca <kmgorczyca@wp.pl>
2020-11-06 12:46:37 +0100

    Modified templates to display posts from database

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')s%&h)6h-^3rmf_7v$-2wwd@56m^d%qem0o6+rne_ff19cgibe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
